a9522e877f9d07061b7a874f1f8411082e9dcde4
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2021-07-30 19:56:44 +0100

    Continued to work on the D&D#1.

--- index.js
+++ index.js
@@ -1,60 +1,51 @@
 const Dice = function(seed) {
+	multiply = (nums) => { return parseInt(nums[0]) * parseInt(nums[1]); }
 
-	// Always remember BIDMAS guys ;)
+	add = (nums) => {	return parseInt(nums[0]) + parseInt(nums[1]);	}
 
-	multiply = (nums) => {
-		return parseInt(nums[0]) * parseInt(nums[1]);
+	sub = (nums) => { return parseInt(nums[0]) - parseInt(nums[1]); }
+
+	random = (min = 1, max, doMax = false) => {
+		if (!seed) {
+			return min !== max || !doMax ? Math.floor(Math.random() * (max - min) + min) : max;
+		} else {
+			return max;
+		}
 	}
 
-	add = (nums) => {
-		return parseInt(nums[0]) + parseInt(nums[1]);
-	}
+	diceRoll = (arr) => {
+		let doMax = false;
 
-	sub = (nums) => {
-		return parseInt(nums[0]) - parseInt(nums[1]);
-	}
+		if (!arr[0]) {
+			arr.shift();
 
-	random = (min, max) => {
-		return min !== max ? Math.floor(Math.random() * (max - min) + min) : max;
+			return random(1, parseInt(arr.map((n, i, a) => {
+				if (n) {
+					if (n.match(/[+]/g)) { doMax = true; return add(n.split('+')); }
+					if (n.match(/[-]/g)) { doMax = true; return sub(n.split('-')); }
+	
+					return n;
+				}
+			})), doMax);
+		} else {
+			const times = arr.shift();
+
+			return random(parseInt(times), parseInt(arr.map((n, i, a) => {
+				if (n) {
+					if (n.match(/[+]/g)) { doMax = true; return add(n.split('+')); }
+					if (n.match(/[-]/g)) { doMax = true; return sub(n.split('-')); }
+	
+					return multiply([times, n]);
+				}
+			})), doMax);
+		}
 	}
 	
 	return {
 		roll: (roll) => {
 			const arr = roll.split('d');
 
-			if (!arr[0]) {
-				arr.shift();
-
-				return random(1, parseInt(arr.map((n, i, a) => {
-					if (n) {
-						if (n.match(/[+]/g)) {
-							return add(n.split('+'));
-						}
-		
-						if (n.match(/[-]/g)) {
-							return sub(n.split('-'));
-						}
-		
-						return n;
-					}
-				})));
-			} else {
-				const times = arr.shift();
-
-				return random(parseInt(times), parseInt(arr.map((n, i, a) => {
-					if (n) {
-						if (n.match(/[+]/g)) {
-							return add(n.split('+'));
-						}
-		
-						if (n.match(/[-]/g)) {
-							return sub(n.split('-'));
-						}
-		
-						return multiply([times, n]);
-					}
-				})));
-			}
+			return diceRoll(arr);
 		}
 	};
 };
@@ -62,6 +53,7 @@ const Dice = function(seed) {
 const d = new Dice();
 
 // Normal Tests
+console.log('Normal Tests!');
 console.log(d.roll('d1')); // 1 - 1
 console.log(d.roll('d20')); // 1 - 20
 console.log(d.roll('d10')); // 1 - 10
@@ -69,6 +61,7 @@ console.log(d.roll('d100')); // 1 - 100
 console.log(d.roll('d1000')); // 1 - 1000
 
 // Addition & Subtraction Tests
+console.log('Addition & Subtraction Tests!');
 console.log(d.roll('d1+10')); // 11 - 11
 console.log(d.roll('d20+5')); // 6 - 25
 console.log(d.roll('d10+5')); // 6 - 15
@@ -76,6 +69,7 @@ console.log(d.roll('d100+5')); // 6 - 105
 console.log(d.roll('d1-0+5')); // 6 - 6
 
 // Multiple Dice Tests
+console.log('Multiple Dice Tests!');
 console.log(d.roll('5d1')); // 5 - 5
 console.log(d.roll('5d20')); // 5 - 100
 console.log(d.roll('2d10')); // 2 - 20
@@ -85,6 +79,7 @@ console.log(d.roll('1000d5')); // 1000 - 5000
 // Seed Tests
 const d1 = new Dice('seed1'), 
 			d2 = new Dice('seed1');
+console.log('Seed Tests!');
 console.log(`d1: ${d1.roll('d10')}, d2: ${d2.roll('d10')}`);
 console.log(`d1: ${d1.roll('d10')}, d2: ${d2.roll('d10')}`);
 console.log(`d1: ${d1.roll('d100')}, d2: ${d2.roll('d100')}`);
